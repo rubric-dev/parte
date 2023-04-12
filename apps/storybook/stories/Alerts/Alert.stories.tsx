b6e3c56b5cdf62f06c2ee0f3c7473f770d6dd2b9
June Kim <57249866+june6723@users.noreply.github.com>
2023-04-12 15:24:41 +0900

    💥 node v18 LTS로 변경 & Storybook v7 upgrade (#11)
    * :arrow_up: node version to 18
    
    * :boom: Storybook 7 migration
    
    * :memo: 스토리북 7버전에 맞는 CSF, MDX 예시
    
    * :wrench: nvm 사용시 버전 명시

---
+++ apps/storybook/stories/Alerts/Alert.stories.tsx
@@ -0,0 +1,49 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Alert } from "@parte-ds/ui";
+
+const AlertStory: Meta = {
+  title: "Components/Alerts/Alert",
+  component: Alert,
+  parameters: {
+    layout: "centered",
+    viewport: "responsive",
+  },
+};
+export default AlertStory;
+
+type Story = StoryObj<typeof Alert>;
+
+export const Default: Story = {
+  args: {
+    type: "alert-inline",
+    status: "error",
+    message: "Warning message.",
+  },
+};
+
+export const BasicAlert: Story = {
+  args: {
+    type: "alert",
+    status: "error",
+    title: "Error Message",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
+  },
+};
+
+export const BasicAlertNoDescription: Story = {
+  args: {
+    type: "alert",
+    status: "success",
+    title: "Success Message",
+  },
+};
+export const ToastAlert: Story = {
+  args: {
+    type: "toast",
+    status: "info",
+    title: "Info Message",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
+  },
+};
