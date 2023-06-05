5d89a5f7ffb443660531a1e5fe1bfe72ea6685f1
dohye1 <tsc03018@gmail.com>
2023-06-05 16:25:31 +0900

    :bug: button click event prevent bubbling

--- packages/parte-ui/src/IconButton/IconButton.tsx
+++ packages/parte-ui/src/IconButton/IconButton.tsx
@@ -3,13 +3,17 @@ import { StyledButton } from "./IconButton.styled";
 import { IconButtonProps } from "./IconButton.types";
 
 export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
-  ({ Icon, variant = "fill-primary", ...props }, ref) => {
+  ({ Icon, variant = "fill-primary", onClick, ...props }, ref) => {
     return (
       <StyledButton
         iconColor={Icon.props.color}
         variant={variant}
-        {...props}
         ref={ref}
+        onClick={(e) => {
+          e.stopPropagation();
+          onClick?.(e);
+        }}
+        {...props}
       >
         {Icon}
       </StyledButton>
