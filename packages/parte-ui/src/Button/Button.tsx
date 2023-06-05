c8a99d03595ae4d3ea6ea8115dc8bf60e327a6b7
dohye1 <tsc03018@gmail.com>
2023-06-05 16:22:46 +0900

    :sparkles: button에 forwardRef 적용

--- packages/parte-ui/src/Button/Button.tsx
+++ packages/parte-ui/src/Button/Button.tsx
@@ -1,26 +1,38 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, forwardRef } from "react";
 import { StyledButton } from "./Button.styled";
 import { ButtonProps } from "./Button.types";
 
-export const Button = ({
-  children,
-  leadingIcon,
-  trailingIcon,
-  variant = "fill-primary",
-  direction = "horizontal",
-  ...props
-}: ButtonProps) => {
-  return (
-    <StyledButton variant={variant} direction={direction} {...props}>
-      {leadingIcon &&
-        React.cloneElement(leadingIcon as ReactElement, {
-          size: direction === "horizontal" ? 12 : 16,
-        })}
-      {children}
-      {trailingIcon &&
-        React.cloneElement(trailingIcon as ReactElement, {
-          size: direction === "horizontal" ? 12 : 16,
-        })}
-    </StyledButton>
-  );
-};
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      leadingIcon,
+      trailingIcon,
+      variant = "fill-primary",
+      direction = "horizontal",
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <StyledButton
+        variant={variant}
+        direction={direction}
+        {...props}
+        ref={ref}
+      >
+        {leadingIcon &&
+          React.cloneElement(leadingIcon as ReactElement, {
+            size: direction === "horizontal" ? 12 : 16,
+          })}
+        {children}
+        {trailingIcon &&
+          React.cloneElement(trailingIcon as ReactElement, {
+            size: direction === "horizontal" ? 12 : 16,
+          })}
+      </StyledButton>
+    );
+  }
+);
+
+Button.displayName = "Button";
--- packages/parte-ui/src/IconButton/IconButton.tsx
+++ packages/parte-ui/src/IconButton/IconButton.tsx
@@ -1,10 +1,20 @@
+import { forwardRef } from "react";
 import { StyledButton } from "./IconButton.styled";
 import { IconButtonProps } from "./IconButton.types";
 
-export const IconButton = ({ Icon, ...props }: IconButtonProps) => {
-  return (
-    <StyledButton iconColor={Icon.props.color} {...props}>
-      {Icon}
-    </StyledButton>
-  );
-};
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ Icon, variant = "fill-primary", ...props }, ref) => {
+    return (
+      <StyledButton
+        iconColor={Icon.props.color}
+        variant={variant}
+        {...props}
+        ref={ref}
+      >
+        {Icon}
+      </StyledButton>
+    );
+  }
+);
+
+IconButton.displayName = "IconButton";
