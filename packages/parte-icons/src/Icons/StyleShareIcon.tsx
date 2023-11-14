c9999db1a2cb035f92818bc6c7cb57ac1edbb573
june6723 <june6723@naver.com>
2023-11-14 17:33:04 +0900

    :adhesive_bandage: JSX Attribute 수정

--- packages/parte-icons/src/Icons/StyleShareIcon.tsx
+++ packages/parte-icons/src/Icons/StyleShareIcon.tsx
@@ -13,23 +13,23 @@ function Icon({ size, ...rest }: Pick<IconProps, "style" | "color" | "size">) {
       <path
         d="M9.33325 2H13.9999V6.66667"
         stroke="currentcolor"
-        stroke-width="1.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M14 9.82457V13C14 13.5523 13.5523 14 13 14H3C2.44772 14 2 13.5523 2 13V3C2 2.44772 2.44772 2 3 2H6"
         stroke="currentcolor"
-        stroke-width="1.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7 9L14 2"
         stroke="currentcolor"
-        stroke-width="1.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- packages/parte-ui/src/@foundations/styleReset.ts
+++ packages/parte-ui/src/@foundations/styleReset.ts
@@ -145,7 +145,7 @@ export const styleReset = css`
   button,
   code,
   body {
-    font-family: Pretendard Variable, Pretendard, -apple-system,
+    font-family: Pretendard Variable, Pretendard, Pretendard JP, -apple-system,
       BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
       "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
       "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
