56aee94919e6097527da4ef1d215f4ed164a25cf
dohye1 <tsc03018@gmail.com>
2024-01-25 09:16:12 +0900

    :lipstick: style picture icon 사이즈 오류

--- packages/parte-icons/src/Icons/StylePictureIcon.tsx
+++ packages/parte-icons/src/Icons/StylePictureIcon.tsx
@@ -5,7 +5,7 @@ function Icon({ size, ...rest }: Pick<IconProps, "style" | "color" | "size">) {
     <svg
       width={size}
       height={size}
-      viewBox="0 0 16 16"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...rest}
