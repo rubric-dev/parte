a87ec2eff8347c1d28e2f53e34b16a73fd03c129
백소원 <dnjs0236@gmail.com>
2024-02-27 23:42:27 +0900

    :lipstick: PreviewCloseIcon 컬러 수정

--- apps/web/CHANGELOG.md
+++ apps/web/CHANGELOG.md
@@ -1,5 +1,12 @@
 # @parte-ds/web
 
+## 0.1.38
+
+### Patch Changes
+
+- Updated dependencies
+  - @parte-ds/icons@1.8.1
+
 ## 0.1.37
 
 ### Patch Changes
--- packages/parte-icons/src/Icons/PreviewCloseOneIcon.tsx
+++ packages/parte-icons/src/Icons/PreviewCloseOneIcon.tsx
@@ -18,14 +18,14 @@ function Icon({ size, ...rest }: Pick<IconProps, "style" | "color" | "size">) {
       />
       <path
         d="M6.77107 6.87372C6.49904 7.17032 6.33301 7.56575 6.33301 7.99995C6.33301 8.92042 7.07921 9.66662 7.99967 9.66662C8.45391 9.66662 8.86571 9.48488 9.16634 9.19018"
-        stroke="v"
+        stroke="currentcolor"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M14 14L2 2"
-        stroke="v"
+        stroke="currentcolor"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
