3d7b2697ef5ff0b401d485d83222bed2a96ef9c7
백소원 <dnjs0236@gmail.com>
2024-03-25 10:06:41 +0900

    :lipstick: 배지 컬러 수정

--- packages/parte-ui/src/@foundations/Badge/badge.ts
+++ packages/parte-ui/src/@foundations/Badge/badge.ts
@@ -6,39 +6,39 @@ export type BadgeColors = keyof BadgeColor | "AUTO";
 
 export const BADGE_COLOR = {
   NEUTRAL: css`
-    background-color: ${COLORS.N200};
+    background-color: ${COLORS.N100};
     color: ${COLORS.N700};
   `,
   BLUE: css`
-    background-color: ${COLORS.B100};
+    background-color: ${COLORS.B50};
     color: ${COLORS.B400};
   `,
   GREEN: css`
-    background-color: ${COLORS.G100};
+    background-color: ${COLORS.G50};
     color: ${COLORS.G500};
   `,
   YELLOW: css`
-    background-color: ${COLORS.Y100};
+    background-color: ${COLORS.Y50};
     color: ${COLORS.Y500};
   `,
   RED: css`
-    background-color: ${COLORS.R100};
+    background-color: ${COLORS.R50};
     color: ${COLORS.R400};
   `,
   VIOLET: css`
-    background-color: ${COLORS.V100};
+    background-color: ${COLORS.V50};
     color: ${COLORS.V400};
   `,
   TEAL: css`
-    background-color: ${COLORS.T100};
+    background-color: ${COLORS.T50};
     color: ${COLORS.T400};
   `,
   PINK: css`
-    background-color: ${COLORS.P100};
+    background-color: ${COLORS.P50};
     color: ${COLORS.P400};
   `,
   ORANGE: css`
-    background-color: ${COLORS.O100};
+    background-color: ${COLORS.O50};
     color: ${COLORS.O400};
   `,
 } as const;
